07295575c594e100eb1fac8f281fb4d37c6b1815
BarathKV <kvbarath2004@gmail.com>
2026-02-28 11:15:25 +0530

    name refactoring

--- functions/maths/convert_ength.py
+++ functions/maths/convert_ength.py
@@ -2,13 +2,21 @@ def convert_length(value:float, from_unit:str, to_unit:str) -> float:
     # Define conversion factors (in meters)
     conversion_factors = {
         'm': 1.0,
+        'metre': 1.0,
         'km': 1000.0,
+        'kilometer': 1000.0,
         'cm': 0.01,
+        'centimeter': 0.01,
         'mm': 0.001,
+        'millimeter': 0.001,
         'in': 0.0254,
+        'inches' : 0.0254,
         'ft': 0.3048,
+        'feet': 0.3048,
         'yd': 0.9144,
+        'yard': 0.9144,
         'mi': 1609.34,
+        'mile': 1609.34,
     }
     
     # Check if the units are valid
